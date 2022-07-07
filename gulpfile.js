9462c50cbe9a470c3b005b7fbcb683ae6eb6cd27
Eider Prieto <prietoeider1@gmail.com>
2022-07-07 08:54:18 -0500

    Creation scafolding

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+const {src, dest, watch} = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const plumber = requiere('gulp-plumber');
+
+function css(done){
+    // identificar el archivo de SASS
+    //Compilarlo
+    //Almacenarla en el Discoduro
+
+    src('src/scss/**/*.scss')
+        .pipe(plumber())//evita errores y detencion del workflow
+        .pipe(sass())  //compilarlo
+        .pipe(dest('build/css'));//almacenarla en el disco duro
+
+
+
+    done();
+}
+function dev(done){
+    watch('src/scss/**/*.scss',css);
+
+    done();
+}
+
+exports.css = css;
+exports.dev = dev;
