bfc023099dc86dd72c20b7c5006b72a227e3798f
Eider Prieto <prietoeider1@gmail.com>
2022-07-08 15:24:32 -0500

    adding mixins and dependency to create webp images

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,15 @@
-const {src, dest, watch} = require('gulp');
+const {src, dest, watch, parallel} = require('gulp');
+
+//CSS
 const sass = require('gulp-sass')(require('sass'));
 const plumber = require('gulp-plumber');
+
+
+//IMAGENES
+    const cache = require('gulp-cache');
+    const imagemin = require('gulp-imagemin');
+    const webp = require('gulp-webp');
+    const avif = require('gulp-avif');
 
 function css(done){
     // identificar el archivo de SASS
@@ -18,9 +27,47 @@ function css(done){
 }
 function dev(done){
     watch('src/scss/**/*.scss',css);
+    done();
+}
+
+function imagenes(done){
+
+    const opciones = {
+        optimizationLevel: 3
+    }
+    src('src/img/**/*.{png,jpg}')//ubicar el archivo sobre el que queremos trabajar
+        .pipe(cache(imagemin(opciones)))
+        .pipe(dest('build/img'))
 
     done();
 }
 
+function versionWbp(done){
+    
+    const opciones ={
+        quality: 50
+    };
+
+    src('src/img/**/*.{png,jpg}')//ubicar el archivo sobre el que queremos trabajar
+        .pipe(webp(opciones))//ejecutar la funcion webp sobre el archivo, la funcion recibe un objeto que lee para fijar los parametros de ejecucion
+        .pipe(dest('build/img'))//guardar en memoria en la carpeta de preferencia
+    
+    done();
+}
+function versionAvif(done){
+    
+    const opciones ={
+        quality: 50
+    };
+
+    src('src/img/**/*.{png,jpg}')//ubicar el archivo sobre el que queremos trabajar
+        .pipe(avif(opciones))//ejecutar la funcion webp sobre el archivo, la funcion recibe un objeto que lee para fijar los parametros de ejecucion
+        .pipe(dest('build/img'))//guardar en memoria en la carpeta de preferencia
+    
+    done();
+}
+exports.imagenes = imagenes;
+exports.versionAvif = versionAvif;
 exports.css = css;
-exports.dev = dev;
+exports.versionWbp = versionWbp;
+exports.dev = parallel(imagenes,versionWbp,avif,dev);
